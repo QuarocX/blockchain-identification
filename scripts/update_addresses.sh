b22d1198567f091f35077c1220f4873bfaed6060
Tarek Galal <tare2.galal@gmail.com>
2021-05-14 19:19:33 +0200

    [tools/update_addresses] support multi file input

--- scripts/update_addresses.sh
+++ scripts/update_addresses.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# usage update_addresses.sh HTML_PAGE CTRL_ADDR CRYPTO_ADDR
-# example: update_addresses.sh stockapp/admin.html \
+# usage update_addresses.sh COMMA_SEP_FILES CTRL_ADDR CRYPTO_ADDR
+# example: update_addresses.sh stockapp/admin.html,stockapp/vote.html \
 #	0x151cb2cf75e266C5491EC3C0aec911fEDa7612A8 0x3009324319C2A35a22BbB1A010671C64dDd82EcC
 
-sed -E "s/voting\.at\((.*)\)/voting\.at\(\"$2\")/g" $1 -i
-sed -E "s/contract\.at\((.*)\)/contract\.at\(\"$3\")/g" $1 -i
+IFS=',' ;for i in $1; do
+    sed -E "s/voting\.at\((.*)\)/voting\.at\(\"$2\")/g" $i -i
+    sed -E "s/contract\.at\((.*)\)/contract\.at\(\"$3\")/g" $i -i
+done
 
