cc2bb5c72fd8a4d47fb0ae3d0224b74686f81d71
Tarek Galal <tare2.galal@gmail.com>
2021-05-16 15:07:35 +0200

    [tools/update_addr] support sed on macOS

--- tools/update_addresses.sh
+++ tools/update_addresses.sh
@@ -5,7 +5,7 @@
 #	0x151cb2cf75e266C5491EC3C0aec911fEDa7612A8 0x3009324319C2A35a22BbB1A010671C64dDd82EcC
 
 IFS=',' ;for i in $1; do
-    sed -E "s/voting\.at\((.*)\)/voting\.at\(\"$2\")/g" $i -i
-    sed -E "s/contract\.at\((.*)\)/contract\.at\(\"$3\")/g" $i -i
+    sed -i -E "s/voting\.at\((.*)\)/voting\.at\(\"$2\")/g" $i
+    sed -i -E "s/contract\.at\((.*)\)/contract\.at\(\"$3\")/g" $i
 done
 
